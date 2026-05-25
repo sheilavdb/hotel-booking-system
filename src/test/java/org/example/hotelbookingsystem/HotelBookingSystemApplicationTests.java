9a59a515aa23d7ae917b509c2a78d10dca243c68
sheilavdb <sheilavdb@gmail.com>
2026-05-25 22:32:36 +0200

    first commit, project setup and dependencies

---
+++ src/test/java/org/example/hotelbookingsystem/HotelBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hotelbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
