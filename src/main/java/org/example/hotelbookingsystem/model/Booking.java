5798475b6de69650741ab1dada81cdb42266edc6
sheilavdb <sheilavdb@gmail.com>
2026-05-25 22:36:19 +0200

    Booking model including validation

---
+++ src/main/java/org/example/hotelbookingsystem/model/Booking.java
@@ -0,0 +1,71 @@
+package org.example.hotelbookingsystem.model;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public class Booking {
+    private int id;
+    @NotBlank(message = "Namn får inte vara tom")
+    private String guestName;
+    @Pattern(regexp = "Enkelrum|Dubbelrum|Svit", message = "Välj mellan: Enkelrum, Dubbelrum, Svit")
+    private String roomType;
+
+    @Min(value = 1, message = "Antal gäster ska vara minst 1")
+    @Max(value = 3, message = "Antal gäster ska inte överstiga 3")
+    private int numberOfGuests;
+// Behövs ingen validering, automatisk beräkning
+    private double totalPrice;
+
+    public Booking() {
+    }
+
+    public Booking(int id, String guestName, String roomType, int numberOfGuests, double totalPrice) {
+        this.id = id;
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.numberOfGuests = numberOfGuests;
+        this.totalPrice = totalPrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getNumberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public void setNumberOfGuests(int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
