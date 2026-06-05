f7deedf0f59e423011e33caadffb1e2872cfa475
sheilavdb <sheilavdb@gmail.com>
2026-06-05 20:05:06 +0200

    changed Rollvalidation with role

--- src/main/java/org/example/hotelbookingsystem/controller/AuthController.java
+++ src/main/java/org/example/hotelbookingsystem/controller/AuthController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -15,17 +17,23 @@ public class AuthController {
 
     private AuthenticationManager authManager;
     private JwtService jwtService;
+    private UserDetailsService userDetailsService;
 
-    public AuthController(AuthenticationManager authManager, JwtService jwtService) {
+    public AuthController(AuthenticationManager authManager, JwtService jwtService, UserDetailsService userDetailsService ) {
         this.authManager = authManager;
         this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest req) {
         try {
             authManager.authenticate(new UsernamePasswordAuthenticationToken(req.getUsername(), req.getPassword()));
-            String token = jwtService.generateToken(req.getUsername(), "USER");
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(req.getUsername());
+            String role = userDetails.getAuthorities().iterator().next().getAuthority();
+
+            String token = jwtService.generateToken(req.getUsername(), role);
             return ResponseEntity.ok(Map.of("token", token));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
